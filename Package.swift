5646f73bb946ffc9c0225c6a81c4441ad2f2f2f8
William Dillon <william@housedillon.com>
2016-03-15 10:42:05 -0700

    Added package swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+//
+// Package.swift
+// NWSObservation
+//
+// Created by William Dillon on 03/14/2016
+// Read LICENSE in the top level directory for further licensing information.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "NWSObservation",
+    dependencies: [
+    ]
+)
+
